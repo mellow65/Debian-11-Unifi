60e730f72841164474b1bb0598770c01eb871771
mellow65 <126365319+mellow65@users.noreply.github.com>
2024-08-07 09:28:16 -0700

    Update prox_lxc_tail_twin.sh

--- prox_lxc_tail_twin.sh
+++ prox_lxc_tail_twin.sh
@@ -1,26 +1,24 @@
+#Wanted to make a script following tailscales recomendations to allow  advertising routes to your home network.
+#I combined the codes from https://tailscale.com/kb/1019/subnets?tab=linux#enable-ip-forwarding
+# and https://tailscale.com/kb/1320/performance-best-practices#ethtool-configuration
 
-lxc.cgroup2.devices.allow: c 10:200 rwm
-lxc.mount.entry: /dev/net/tun dev/net/tun none bind,create=file
+#!/bin/bash
+color='\033[1;31m'
 
+echo -e "${color}Installing ethtool and networkd-dispactcher${reset}"
 
-/etc/pve/lxc/${CTID}.conf
+apt install ethtool -y && apt install networkd-dispatcher -y
 
-apt install ethtool -y
-
-apt install networkd-dispatcher -y
-
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
-
-
+echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
+sysctl -p /etc/sysctl.d/99-tailscale.conf
 
 NETDEV=$(ip route show 8.8.8.8 | cut -f5 -d' ')
 
-sudo ethtool -K eth0 rx-udp-gro-forwarding on rx-gro-list off
+ethtool -K eth0 rx-udp-gro-forwarding on rx-gro-list off
 
-printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip route show 0/0 | cut -f5 -d" ")" | sudo tee /etc/networkd-dispatcher/routable.d/50-tailscale
-sudo chmod 755 /etc/networkd-dispatcher/routable.d/50-tailscale
+printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip route show 0/0 | cut -f5 -d" ")" | tee /etc/networkd-dispatcher/routable.d/50-tailscale
+chmod 755 /etc/networkd-dispatcher/routable.d/50-tailscale
 
 sudo /etc/networkd-dispatcher/routable.d/50-tailscale
 test $? -eq 0 || echo 'An error occurred.'
