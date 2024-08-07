5190174aeb6a8894c35bdf7f541278bff97abe26
mellow65 <126365319+mellow65@users.noreply.github.com>
2024-08-07 10:25:22 -0700

    Update prox_lxc_tail_twin.sh

--- prox_lxc_tail_twin.sh
+++ prox_lxc_tail_twin.sh
@@ -4,29 +4,29 @@
 
 #!/bin/bash
 color='\033[1;31m'
+reset='\033[0m'
 
 echo -e "${color}Installing ethtool and networkd-dispactcher${reset}"
-sleep 1
+sleep 2
 apt install ethtool -y && apt install networkd-dispatcher -y
 
 echo -e "${color}Making 99-tailscale.conf, adding ipv4 and ipv6 forwarding rules.${reset}"
-sleep 1
+sleep 2
 echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
 sysctl -p /etc/sysctl.d/99-tailscale.conf
 
 echo -e "${color}Setting up to survive reboot.${reset}"
-sleep 1
+sleep 2
 ethtool -K eth0 rx-udp-gro-forwarding on rx-gro-list off
-
-printf '#!/bin/sh\n\nethtool -K %s rx-udp-gro-forwarding on rx-gro-list off \n' "$(ip route show 0/0 | cut -f5 -d" ")" | tee /etc/networkd-dispatcher/routable.d/50-tailscale
+echo $'#!/bin/sh\nethtool -K eth0 rx-udp-gro-forwarding on rx-gro-list off' | tee /etc/networkd-dispatcher/routable.d/50-tailscale
 chmod 755 /etc/networkd-dispatcher/routable.d/50-tailscale
 
 
 echo -e "${color}Testing.${reset}"
-sleep 1
+sleep 2
 /etc/networkd-dispatcher/routable.d/50-tailscale
 test $? -eq 0 || echo 'An error occurred.'
 
-echo -e "${color}If no error shown, everything worked.${reset}"
+echo -e "${color}If no error shown, everything probably worked.${reset}"
 
