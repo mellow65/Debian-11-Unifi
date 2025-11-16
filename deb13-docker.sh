3365afcfccc4f6dca46c3e3d54e3a6581d22b0e5
mellow65 <126365319+mellow65@users.noreply.github.com>
2025-11-16 10:46:31 -0800

    Modify Docker repository setup in deb13-docker.sh
    Updated Docker repository configuration for Debian.

--- deb13-docker.sh
+++ deb13-docker.sh
@@ -7,21 +7,27 @@ reset='\033[0m'
 
 echo -e "${color}By using this script, you'll update the system, install ca-certificates, curl, docker-ce, docker-ce-cli, containerd.io, docker-buildx-plugin, and docker-compose-plugin.${reset}"
 
-sudo apt-get update -y
-sudo apt-get install ca-certificates curl -y
+sudo apt update -y
+sudo apt install ca-certificates curl -y
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
 echo -e "${color}Step 4/6: Adding Docker repository...${reset}"
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
 echo -e "${color}Step 5/6: Updating package list again...${reset}"
-sudo apt-get update
+sudo apt update
 
 echo -e "${color}Step 6/6: Installing Docker and plugins...${reset}"
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo -e "${color}Testing docker was installed correctly${reset}"
 sudo docker run hello-world
